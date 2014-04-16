dfc7107cfd1d98610ce79a03c51d143cd223595b
Jansen Felipe <jansen.felipe@gmail.com>
2014-04-16 01:32:42 -0300

    First Commit

---
+++ src/com/artifex/mupdflib/CallbackApplication.java
@@ -0,0 +1,23 @@
+package com.artifex.mupdflib;
+
+public abstract class CallbackApplication {
+
+	public interface MuPDFCallbackInterface {
+		public void callbackMethod(String str);
+	}
+
+	public final static class MuPDFCallbackClass {
+		private static String gaiStr = "";
+		public static MuPDFCallbackInterface callback;
+
+		public MuPDFCallbackClass() {
+		}
+
+		static void sendGaiView(String str) {
+			gaiStr = str;
+			if (callback != null)
+				callback.callbackMethod(gaiStr);
+		}
+	}
+
+}
